e0b9772701df0624ee099b90f3401405aac03024
Carlos Alberto Carneiro <lionbllack31@gmail.com>
2024-09-28 13:35:15 -0300

    adicionando imagens e fontes

--- src/Components/NavBar/navBar.jsx
+++ src/Components/NavBar/navBar.jsx
@@ -2,6 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { useState,useEffect } from 'react';
+import Logo from '../../assets/img/logo.svg';
 
 function Navigation() {
     const [activeLink, setactiveLink] = useState('home');//atualição do valor link
