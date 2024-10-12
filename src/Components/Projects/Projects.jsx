3f3063f62d6b3a7f6a8bffa5e01cf8cf1fcd6dd6
Carlos Alberto Carneiro <lionbllack31@gmail.com>
2024-10-12 18:55:42 -0300

    adicionando aba Projects

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,4 +1,35 @@
+import { Container, Row,Col } from "react-bootstrap"
+
 function Projects (){
 
+    const projects = [
+        
+        
+            {
+                title: "",
+                description: "",
+                imgUrl: ""
+            },
+
+            {
+
+            }
+        
+        
+
+    ]
+    return(
+        <section className="project" id="project">
+            <Container>
+                <Row>
+                    <Col>
+                        <h2>
+                        Projects</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit modi, earum ut inventore, sit sed harum sunt nostrum expedita soluta ad consectetur doloribus eum mollitia ex, possimus dolore doloremque corrupti.</p>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    )
 }
 export default Projects
