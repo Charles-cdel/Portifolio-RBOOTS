fd2590589330a261a54509d174c4ca2820910ca0
Carlos Alberto Carneiro <lionbllack31@gmail.com>
2024-10-12 19:18:34 -0300

    adicionando Nav Bootstrap

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,4 +1,4 @@
-import { Container, Row,Col } from "react-bootstrap"
+import { Container, Row,Col, NavItem, NavLink, Nav } from "react-bootstrap"
 
 function Projects (){
 
@@ -26,6 +26,23 @@ function Projects (){
                         <h2>
                         Projects</h2>
                         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit modi, earum ut inventore, sit sed harum sunt nostrum expedita soluta ad consectetur doloribus eum mollitia ex, possimus dolore doloremque corrupti.</p>
+                        <Nav variant = "pills" defaultActiveKey ="/home" >
+                            <Nav.Item>
+                                <Nav.Link eventKey = "first">
+                                    Tab One
+                                </Nav.Link>
+                                <Nav.Item>
+                                    <Nav.Link eventKey = "second">
+                                        Tab Two
+                                    </Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey = "third">
+                                        Tab Three
+                                    </Nav.Link>
+                                </Nav.Item>
+                            </Nav.Item>
+                        </Nav>
                     </Col>
                 </Row>
             </Container>
